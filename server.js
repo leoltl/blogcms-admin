ae275934452c28b1b3358e9f16a501813518b100
Leo Lee <tl.leolee@gmail.com>
2020-09-20 08:16:49 -0700

    update built app path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
 app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || 80;
 app.listen(port);
